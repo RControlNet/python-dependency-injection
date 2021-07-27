89ccf690f68ba275f9c14dd1617c8e0ceefe82c6
Mayank <mayank31313@gmail.com>
2021-07-28 00:09:47 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from annotations import Bean, Autowired, AppInitilizer
+
+class TestBean:
+    def __init__(self, name):
+        self.name = name
+
+
+@Bean()
+def getTestBean() -> TestBean:
+    return TestBean("Test 123")
+
+testBean = None
+
+app = AppInitilizer()
+if __name__ == "__main__":
+    @Autowired()
+    def setTestBean(bean: TestBean):
+        global testBean
+        testBean = bean
+
+    app.run()
+
+    print(testBean.name)
