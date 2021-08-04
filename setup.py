8ca5502e42468f7e221e373b64384d2197ed019b
Mayank <mayank31313@gmail.com>
2021-08-04 12:53:15 +0530

    setup.py added

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pydependencyinjection",
+    version="0.0.1",
+    author="Mayank Shinde",
+    author_email="mayank31313@gmail.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD 3-Clause License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
