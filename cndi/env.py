b1de22a40db605adde6ecbb06d5d26e28acbc817
Mayank <mayank31313@gmail.com>
2021-08-18 20:06:42 +0530

    properties added

---
+++ cndi/env.py
@@ -0,0 +1,23 @@
+from yaml import load, Loader
+import os
+
+
+DEFAULT_PROPERTIES_PATH = "./resources/application.yml"
+
+class Properties:
+    def __init__(self, property_file):
+        if(not os.path.exists(property_file)):
+            raise FileNotFoundError(f"Environement file does not exists at {property_file}")
+
+        with open(property_file, "r") as stream:
+            self.data = load(stream, Loader)
+
+    def getProperty(self, key):
+        return self.data[key]
+
+    @staticmethod
+    def getResourceProperties():
+        if not os.path.exists(DEFAULT_PROPERTIES_PATH):
+            return None
+
+        return Properties(DEFAULT_PROPERTIES_PATH)
